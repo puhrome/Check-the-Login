0bd325e57089e56b80ef4a2c830cfc45e72a77b6
puhrome <phirom.yim@fullsail.edu>
2014-04-16 21:39:41 -0400

    Check the Login
    Conditional Statement using Multiple Results in WebStorm

---
+++ js/script.js
@@ -0,0 +1,26 @@
+/**
+ * Check the Login Created by Phirom Yim on 4/16/14.
+ */
+//Conditional Statement using Multiple Results
+//if the username and password is correct print result
+var userName = String(prompt("Type in your Username"));
+userName = "Senor Meow";
+if(userName >= "Senor Meow"){
+    String (prompt("Type in your password"));
+
+}else{userName != "Senor Meow";
+
+
+
+
+}
+var password = "catnip!";
+
+if(password >= "catnip!") {
+    console.log("Welcome," + (userName) + "!");
+
+}
+if(password <= "catnip!"){
+        console.log("User not found. Try again.");
+
+}
